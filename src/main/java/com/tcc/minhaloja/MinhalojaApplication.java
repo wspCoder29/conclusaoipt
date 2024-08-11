bfa207e86fc35323f1307923ab078cce58e383ed
Leandro <leandro.wsp@gmail.com>
2024-08-10 22:28:38 -0300

    config inicial

---
+++ src/main/java/com/tcc/minhaloja/MinhalojaApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.minhaloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhalojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhalojaApplication.class, args);
+	}
+
+}
